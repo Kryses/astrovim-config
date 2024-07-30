6c88256a81df4ae497a0d6792a1742211bb11db6
Chad Provencher <thechad802@gmail.com>
2024-07-30 11:40:58 -0400

    removed ai

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -2,8 +2,6 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.git.octo-nvim" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.git.neogit" },
